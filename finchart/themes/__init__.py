14a0ef916648b201b2cf6efd7df196f7b45dc1f3
Rajeev84 <91060507+Rajeev84@users.noreply.github.com>
2026-08-03 21:41:14 +0530

    Delete finchart directory

--- finchart/themes/__init__.py
+++
@@ -1,4 +0,0 @@
-"""finchart.themes - Color themes and visual style configurations."""
-from .style import Theme, DarkTheme, LightTheme
-
-__all__ = ["Theme", "DarkTheme", "LightTheme"]
--- finchart/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-"""finchart.utils - Shared mathematical utilities for FinChart."""
-from .math import nice_number, nice_bar_step, format_price, clamp
-
-__all__ = ["nice_number", "nice_bar_step", "format_price", "clamp"]
