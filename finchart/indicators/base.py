bf9ede482dcda113c08f4db3103309a53f8f44f7
Rajeev84 <91060507+Rajeev84@users.noreply.github.com>
2026-08-03 22:40:00 +0530

    Delete finchart directory

--- finchart/indicators/base.py
+++
@@ -1,5 +0,0 @@
-"""Indicator Base Specification - Placeholder for new BaseIndicator plugin system.
-
-This module is being refactored to support EasyPyChart-compatible BaseIndicator plugin system.
-The old Indicator class has been removed as part of the indicator architecture overhaul.
-"""
